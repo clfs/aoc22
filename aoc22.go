7d50246ec9810b50976eea8579766eda74b87649
Calvin Figuereo-Supraner <mail@calvin.page>
2022-12-11 09:52:22 -0800

    Add ReadInts utility

--- aoc22.go
+++ aoc22.go
@@ -1,7 +1,10 @@
 package aoc22
 
 import (
+	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"testing"
 )
 
@@ -12,4 +15,20 @@ func ReadTestFile(t *testing.T, path string) []byte {
 		t.Fatalf("failed to read test data: %v", err)
 	}
 	return data
+}
+
+var intsRegexp = regexp.MustCompile(`\d+`)
+
+// ReadInts returns a slice of all integers in the given string. For example,
+// "a 12\n3, b::4" becomes []int{12, 3, 4}.
+func ReadInts(s string) []int {
+	var result []int
+	for _, num := range intsRegexp.FindAllString(s, -1) {
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			panic(fmt.Sprintf("bad number: %q", num))
+		}
+		result = append(result, n)
+	}
+	return result
 }
