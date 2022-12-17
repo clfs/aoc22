296233171872a20c41da9df43146c6e7c073842c
Calvin Figuereo-Supraner <mail@calvin.page>
2022-12-16 17:36:51 -0800

    Add day 16 tmp work

--- day16/day16.go
+++ day16/day16.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"golang.org/x/exp/slices"
@@ -14,193 +16,250 @@ import (
 type Valve struct {
 	Name    string
 	Rate    int
-	Open    bool
 	Tunnels []string
 }
 
 var ValveRegexp = regexp.MustCompile(`Valve ([A-Z]{2}) has flow rate=(\d+); (?:tunnels lead|tunnel leads) to (?:valves|valve) ([A-Z]{2}(?:, [A-Z]{2})*)`)
 
 func (v *Valve) UnmarshalText(text []byte) error {
-	// Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 	matches := ValveRegexp.FindStringSubmatch(string(text))
 	if matches == nil {
 		return fmt.Errorf("no match for %q", text)
 	}
+
 	v.Name = matches[1]
-	fmt.Sscanf(matches[2], "%d", &v.Rate)
 	v.Tunnels = strings.Split(matches[3], ", ")
-	return nil
+
+	var err error
+	v.Rate, err = strconv.Atoi(matches[2])
+	return err
 }
 
 type Volcano struct {
-	Valves      map[string]Valve
-	Location    string
-	Released    int
-	TimeLimit   int
-	TimeElapsed int
+	Nodes  []string            // The valves.
+	Edges  map[string][]string // A map from valves to their connected valves.
+	Rates  map[string]int      // A map from valves to their flow rate.
+	Status map[string]bool     // A map from valves to their open status. Closed is false.
+
+	Location string // The valve you're standing at.
+
+	TimeElapsed int // How many minutes have passed.
+	TimeLimit   int // The time limit before the volcano explodes.
+
+	// Cached results for Volcano.LenPath.
+	LenPathCache map[string]map[string]int
 }
 
-func NewVolcano(valves []Valve, timeLimit int) *Volcano {
-	m := make(map[string]Valve)
-	for _, valve := range valves {
-		m[valve.Name] = valve
+func NewVolcano(vs []Valve, limit int) *Volcano {
+	volcano := &Volcano{
+		Edges:        make(map[string][]string),
+		Rates:        make(map[string]int),
+		Status:       make(map[string]bool),
+		Location:     "AA",
+		TimeLimit:    limit,
+		LenPathCache: make(map[string]map[string]int),
 	}
-	return &Volcano{
-		Valves:    m,
-		Location:  "AA",
-		TimeLimit: timeLimit,
+
+	for _, v := range vs {
+		volcano.Nodes = append(volcano.Nodes, v.Name)
+
+		// For easier testing, sort the tunnels alphabetically.
+		slices.Sort(v.Tunnels)
+		volcano.Edges[v.Name] = append(volcano.Edges[v.Name], v.Tunnels...)
+
+		volcano.Rates[v.Name] = v.Rate
+		volcano.Status[v.Name] = false
+	}
+
+	return volcano
+}
+
+func (v *Volcano) Rate(name string) int {
+	rate, ok := v.Rates[name]
+	if !ok {
+		panic("bad name " + name)
+	}
+	return rate
+}
+
+func (v *Volcano) Open(name string) {
+	status, ok := v.Status[name]
+	if !ok {
+		panic("bad name " + name)
+	}
+	if status {
+		panic(name + " already open")
+	}
+	v.Status[name] = true
+}
+
+func (v *Volcano) IsOpen(name string) bool {
+	status, ok := v.Status[name]
+	if !ok {
+		panic("bad name " + name)
+	}
+	return status
+}
+
+func (v *Volcano) IsClosed(name string) bool {
+	return !v.IsOpen(name)
+}
+
+// NextFrom returns the valves that are next from the given valve.
+func (v *Volcano) NextFrom(name string) []string {
+	edges, ok := v.Edges[name]
+	if !ok {
+		panic("bad name " + name)
+	}
+	return edges
+}
+
+// Path returns the shortest path between from and to.
+func (v *Volcano) Path(from, to string) ([]string, bool) {
+	visited := map[string]bool{from: true}
+	queue := [][]string{{from}}
+
+	for len(queue) > 0 {
+		var nextQueue [][]string
+		for _, path := range queue {
+			tail := path[len(path)-1]
+
+			visited[tail] = true
+
+			if tail == to {
+				return path, true
+			}
+
+			for _, next := range v.NextFrom(tail) {
+				if visited[next] {
+					continue
+				}
+				tmp := make([]string, len(path))
+				copy(tmp, path)
+				nextQueue = append(nextQueue, append(tmp, next))
+			}
+		}
+		queue = nextQueue
+	}
+
+	return nil, false
+}
+
+// BestMove returns the best next move. It returns either the current
+// valve (indicating it should be opened), or the next valve to step to.
+//
+// If it's no longer useful to move, ok is false.
+func (v *Volcano) BestMove() (string, bool) {
+	// Get the shortest path to every useful valve.
+	paths := make(map[string][]string)
+	for _, name := range v.Nodes {
+		if v.IsOpen(name) || v.Rate(name) == 0 {
+			continue // useless
+		}
+		path, ok := v.Path(v.Location, name)
+		if !ok {
+			continue // unreachable
+		}
+		paths[name] = path
+	}
+
+	// Find the path with the best score.
+	var (
+		bestScore int
+		bestPath  []string
+	)
+	for _, path := range paths {
+		score := v.Score(path)
+		if score > bestScore {
+			bestScore = score
+			bestPath = path
+		}
+	}
+
+	log.Printf("- best path with score %d: %v", bestScore, bestPath)
+
+	switch l := len(bestPath); l {
+	case 0:
+		return "", false // no more closed valves
+	case 1:
+		return bestPath[0], true // open the current valve
+	default:
+		return bestPath[1], true // move to the next valve
 	}
 }
 
-// Tick advances the volcano simulation by one minute.
-func (v *Volcano) Tick() {
+// Score returns the amount of pressure that would be relieved before
+// the volcano explodes, if you followed the path and opened the valve
+// at the end.
+//
+// If the valve at the end is already open, or if you can't reach the
+// valve before the volcano explodes, the score is 0.
+//
+// If the path is empty, Score panics.
+func (v *Volcano) Score(path []string) int {
+	timeNeeded := len(path)
+	timeLeft := v.TimeLimit - v.TimeElapsed
+
+	tail := path[len(path)-1]
+	tailRate := v.Rate(tail)
+
+	if v.IsOpen(tail) {
+		return 0
+	}
+
+	if timeNeeded >= timeLeft {
+		return 0
+	}
+
+	return (timeLeft - timeNeeded) * tailRate
+}
+
+// Tick advances the simulation by one minute.
+// It returns the pressure released in that minute.
+func (v *Volcano) Tick() int {
 	defer func() { v.TimeElapsed++ }()
 
 	log.Printf("==== minute %d ====", v.TimeElapsed)
 
-	// Release flow from open valves.
-	var released int
-	for _, valve := range v.Valves {
-		if valve.Open {
-			released += valve.Rate
+	var (
+		pressure   int
+		openValves []string
+	)
+	for _, name := range v.Nodes {
+		if v.IsOpen(name) {
+			openValves = append(openValves, name)
+			pressure += v.Rate(name)
 		}
 	}
-	if released > 0 {
-		log.Printf("valve(s) %s released %d pressure", strings.Join(v.openValves(), ", "), released)
-		v.Released += released
+	if len(openValves) > 0 {
+		log.Printf(
+			"open valve(s) %s released %d pressure",
+			strings.Join(openValves, ", "), pressure,
+		)
 	}
 
-	// Find the shortest path to each valve potentially worth opening.
-	var paths [][]string
-	for name, valve := range v.Valves {
-		if valve.Open {
-			continue // already open
-		}
-		if valve.Rate == 0 {
-			continue // useless
-		}
-		path, ok := v.PathTo(name)
-		if !ok {
-			continue // unreachable
-		}
-		paths = append(paths, path)
-	}
-
-	bestPath := v.bestOfPaths(paths)
-
-	// If no worthwhile paths exist, wait out the clock.
-	if bestPath == nil {
-		log.Print("no worthwhile actions found")
-		return
-	}
-
-	// If the best path is to the current location, open the current valve.
-	if len(bestPath) == 1 {
-		log.Printf("opening valve %s", v.Location)
-		v.OpenLocation()
-		return
-	}
-
-	// Otherwise, the best path is to a different location.
-	// Move to the next location in the path.
-	log.Printf("moving to %s", bestPath[1])
-	v.Location = bestPath[1]
-}
-
-// OpenLocation opens the valve at the current location.
-func (v *Volcano) OpenLocation() {
-	named, ok := v.Valves[v.Location]
-	if !ok {
-		panic(fmt.Sprintf("bad location %q", v.Location))
-	}
-	named.Open = true
-	v.Valves[v.Location] = named
-}
-
-// IsOpen returns true if the named valve is open.
-func (v *Volcano) IsOpen(name string) bool {
-	return v.Valves[name].Open
-}
-
-// IsClosed returns true if the named valve is closed.
-func (v *Volcano) IsClosed(name string) bool {
-	return !v.Valves[name].Open
-}
-
-// openValves returns the names of all open valves, in alphabetical order.
-func (v *Volcano) openValves() []string {
-	var open []string
-	for name, valve := range v.Valves {
-		if valve.Open {
-			open = append(open, name)
+	move, ok := v.BestMove()
+	if ok {
+		if move == v.Location {
+			log.Printf("opening valve %s", move)
+			v.Open(move)
+		} else {
+			log.Printf("moving to valve %s", move)
+			v.Location = move
 		}
 	}
-	slices.Sort(open)
-	return open
-}
 
-// bestOfPaths returns the best path to take from multiple options.
-// If no paths are provided, it returns nil.
-func (v *Volcano) bestOfPaths(paths [][]string) []string {
-	var best []string
-	for _, path := range paths {
-		if best == nil || len(path) < len(best) {
-			best = path
-		}
-	}
-	return best
-}
-
-// TimeToOpen returns the time it would take to open the named valve.
-// If the valve is already open, it returns 0.
-// If the valve isn't reachable, ok is false.
-func (v *Volcano) TimeToOpen(name string) (time int, ok bool) {
-	if v.IsOpen(name) {
-		return 0, true
-	}
-
-	path, ok := v.PathTo(name)
-	if !ok {
-		return 0, false
-	}
-
-	return len(path), true
-}
-
-// PathTo returns the shortest path from the current location to the given valve.
-// It includes both endpoints. If no path exists, ok is false.
-func (v *Volcano) PathTo(name string) (path []string, ok bool) {
-	// BFS.
-	visited := make(map[string]bool)
-	queue := [][]string{{v.Location}}
-	for len(queue) > 0 {
-		path := queue[0]
-		queue = queue[1:]
-
-		last := path[len(path)-1]
-		if last == name {
-			return path, true
-		}
-
-		if visited[last] {
-			continue
-		}
-		visited[last] = true
-
-		for _, tunnel := range v.Valves[last].Tunnels {
-			queue = append(queue, append(path, tunnel))
-		}
-	}
-	return nil, false
+	return pressure
 }
 
 // Run runs the simulation until the time limit is reached.
 // It returns the total amount of pressure released.
 func (v *Volcano) Run() int {
+	var sum int
 	for v.TimeElapsed < v.TimeLimit {
-		v.Tick()
+		sum += v.Tick()
 	}
-	return v.Released
+	return sum
 }
 
 func Parse(r io.Reader) ([]Valve, error) {
@@ -221,5 +280,166 @@ func Part1(r io.Reader) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	return NewVolcano(valves, 30).Run(), nil
+	return NewVolcano(valves, 30).Solve(), nil
 }
+
+// RandSample returns a sample of size n from pop. It alters the order
+// of elements in pop.
+func RandSample(pop []string, n int) ([]string, bool) {
+	if n > len(pop) {
+		return nil, false
+	}
+	rand.Shuffle(len(pop), func(i, j int) { pop[i], pop[j] = pop[j], pop[i] })
+	return pop[:n], true
+}
+
+func (v *Volcano) Solve() int {
+	var targets []string
+	for _, name := range v.Nodes {
+		if v.IsClosed(name) && v.Rate(name) > 0 {
+			targets = append(targets, name)
+		}
+	}
+
+	var (
+		bestScore  int
+		bestSample []string
+	)
+
+	n := min(len(targets), 9)
+
+	for i := 0; i < 1000000000; i++ {
+		rand.Shuffle(len(targets), func(i, j int) { targets[i], targets[j] = targets[j], targets[i] })
+		score := v.Evaluate(targets[:n])
+		if score > bestScore {
+			bestScore = score
+			tmp := make([]string, n)
+			copy(tmp, targets[:n])
+			bestSample = tmp
+			log.Printf("score %d with sample %v", bestScore, bestSample)
+		}
+	}
+
+	return bestScore
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// Evaluate determines how much pressure would be released if you
+// visited each target in order and opened them.
+//
+// It returns 0 if you can't reach all targets before the volcano explodes.
+func (v *Volcano) Evaluate(targets []string) int {
+	var (
+		timeElapsed = 0    // how much time has passed
+		burnRate    = 0    // pressure burnt per minute
+		totalBurnt  = 0    // total pressure burnt
+		location    = "AA" // current location
+	)
+
+	for _, target := range targets {
+		// log.Printf("*** %d min elapsed, location = %s", timeElapsed, location)
+
+		// Get the length of the path to the next target.
+		lenPath := v.LenPath(location, target)
+
+		// log.Printf("%d edges to next target", lenPath)
+
+		// If no path exists, or the path would take too long, stand
+		// in place and finish.
+		if lenPath == -1 || timeElapsed+lenPath >= v.TimeLimit {
+			// log.Print("didn't get to all targets...")
+			return 0
+		}
+
+		// Move to the next target.
+		// log.Printf("moving to %s over %d minute(s)", target, lenPath)
+		location = target
+		timeElapsed += lenPath
+		totalBurnt += burnRate * lenPath
+
+		// Open it.
+		totalBurnt += burnRate
+		// log.Printf("opening %s over 1 minute", location)
+		burnRate += v.Rate(location)
+		timeElapsed += 1
+	}
+
+	// Add anything left...
+	// log.Printf("*** %d min elapsed, location = %s", timeElapsed, location)
+	totalBurnt += burnRate * (v.TimeLimit - timeElapsed)
+
+	return totalBurnt
+}
+
+func (v *Volcano) LenPathCached(from, to string) (int, bool) {
+	f, ok := v.LenPathCache[from]
+	if !ok {
+		return 0, false
+	}
+
+	t, ok := f[to]
+	if !ok {
+		return 0, false
+	}
+
+	return t, true
+}
+
+// LenPath returns the number of edges between from and to.
+// If from == to, it returns 0.
+// If no path exists, it returns -1.
+func (v *Volcano) LenPath(from, to string) int {
+	if n, ok := v.LenPathCached(from, to); ok {
+		return n
+	}
+
+	visited := make(map[string]bool)
+	queue := []string{from}
+	count := 0
+
+	for len(queue) > 0 {
+		var nextQueue []string
+		for _, name := range queue {
+			visited[name] = true
+			if name == to {
+				if _, ok := v.LenPathCache[from]; !ok {
+					v.LenPathCache[from] = make(map[string]int)
+				}
+				v.LenPathCache[from][to] = count
+				return count
+			}
+			for _, next := range v.NextFrom(name) {
+				if visited[next] {
+					continue
+				}
+				nextQueue = append(nextQueue, next)
+			}
+		}
+		queue = nextQueue
+		count++
+	}
+
+	if _, ok := v.LenPathCache[from]; !ok {
+		v.LenPathCache[from] = make(map[string]int)
+	}
+	v.LenPathCache[from][to] = -1
+	return -1
+}
+
+/*
+
+new strategy:
+
+- t0, t1, etc.
+- eliminate anything with score zero
+- [t0,t1],[t1,t2],[t2,t3], etc.
+- eliminate anything with score zero
+- if I have nothing left to check, return the best path
+
+*/
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -14,15 +14,15 @@ func TestValve_UnmarshalText(t *testing.T) {
 	}{
 		{
 			"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
-			Valve{"AA", 0, false, []string{"DD", "II", "BB"}},
+			Valve{"AA", 0, []string{"DD", "II", "BB"}},
 		},
 		{
 			"Valve OV has flow rate=10; tunnels lead to valves YW, JT, NN, TK",
-			Valve{"OV", 10, false, []string{"YW", "JT", "NN", "TK"}},
+			Valve{"OV", 10, []string{"YW", "JT", "NN", "TK"}},
 		},
 		{
 			"Valve HH has flow rate=22; tunnel leads to valve GG",
-			Valve{"HH", 22, false, []string{"GG"}},
+			Valve{"HH", 22, []string{"GG"}},
 		},
 	}
 
@@ -53,26 +53,82 @@ func readValves(t *testing.T, name string) []Valve {
 	return valves
 }
 
-func TestVolcano_TimePathTo(t *testing.T) {
+func TestNewVolcano(t *testing.T) {
 	valves := readValves(t, "testdata/small.txt")
-	volcano := NewVolcano(valves, 30)
+	limit := 30
+
+	want := &Volcano{
+		Nodes: []string{"AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH", "II", "JJ"},
+		Edges: map[string][]string{
+			"AA": {"BB", "DD", "II"},
+			"BB": {"AA", "CC"},
+			"CC": {"BB", "DD"},
+			"DD": {"AA", "CC", "EE"},
+			"EE": {"DD", "FF"},
+			"FF": {"EE", "GG"},
+			"GG": {"FF", "HH"},
+			"HH": {"GG"},
+			"II": {"AA", "JJ"},
+			"JJ": {"II"},
+		},
+		Rates: map[string]int{
+			"AA": 0,
+			"BB": 13,
+			"CC": 2,
+			"DD": 20,
+			"EE": 3,
+			"FF": 0,
+			"GG": 0,
+			"HH": 22,
+			"II": 0,
+			"JJ": 21,
+		},
+		Status: map[string]bool{
+			"AA": false,
+			"BB": false,
+			"CC": false,
+			"DD": false,
+			"EE": false,
+			"FF": false,
+			"GG": false,
+			"HH": false,
+			"II": false,
+			"JJ": false,
+		},
+		Location:  "AA",
+		TimeLimit: 30,
+	}
+	got := NewVolcano(valves, limit)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("NewVolcano() mismatch (-want,+got):%s\n", diff)
+	}
+}
+
+func TestVolcano_Path(t *testing.T) {
+	volcano := NewVolcano(readValves(t, "testdata/small.txt"), 30)
 
 	cases := []struct {
-		in   string
-		want []string
+		from, to string
+		want     []string
 	}{
-		{"AA", []string{"AA"}},
-		{"DD", []string{"AA", "DD"}},
-		{"JJ", []string{"AA", "II", "JJ"}},
+		{"AA", "AA", []string{"AA"}},
+		{"AA", "DD", []string{"AA", "DD"}},
+		{"AA", "II", []string{"AA", "II"}},
+		{"AA", "BB", []string{"AA", "BB"}},
+		{"AA", "CC", []string{"AA", "BB", "CC"}},
+		{"GG", "DD", []string{"GG", "FF", "EE", "DD"}},
 	}
 
 	for _, tc := range cases {
-		got, ok := volcano.PathTo(tc.in)
+		got, ok := volcano.Path(tc.from, tc.to)
 		if !ok {
-			t.Errorf("Volcano.PathTo(%q): not ok", tc.in)
+			t.Errorf("Volcano.Path(%q, %q) not ok", tc.from, tc.to)
 		}
 		if diff := cmp.Diff(tc.want, got); diff != "" {
-			t.Errorf("Volcano.PathTo(%q) mismatch (-want,+got):%s\n", tc.in, diff)
+			t.Errorf(
+				"Volcano.Path(%q, %q) mismatch (-want,+got):%s\n",
+				tc.from, tc.to, diff,
+			)
 		}
 	}
 }
@@ -82,8 +138,8 @@ func TestPart1(t *testing.T) {
 		name string
 		want int
 	}{
-		{"testdata/small.txt", 1651},
-		//{"testdata/input.txt", 1},
+		//{"testdata/small.txt", 1651},
+		{"testdata/input.txt", 1},
 	}
 
 	for _, tc := range cases {
@@ -102,5 +158,31 @@ func TestPart1(t *testing.T) {
 				t.Errorf("got %d, want %d", got, tc.want)
 			}
 		})
+	}
+}
+
+func TestVolcano_Evaluate(t *testing.T) {
+	volcano := NewVolcano(readValves(t, "testdata/small.txt"), 30)
+
+	cases := []struct {
+		path []string
+		want int
+	}{
+		// testdata/small.txt
+		{[]string{"DD", "BB", "JJ", "HH", "EE", "CC"}, 1651},
+		{[]string{"DD"}, 560},
+		{[]string{"JJ"}, 567},
+		{[]string{"DD", "BB"}, 33*28 - ((33 - 20) * 3)},
+
+		// testdata/input.txt
+		// {[]string{"FJ", "EL", "ST", "PF", "MD", "DK", "LR"}, 1975},
+		// {[]string{"OV", "FJ", "EL", "ST", "PF", "MD", "DK", "FK", "LR", "XX"}, 0},
+	}
+
+	for _, tc := range cases {
+		got := volcano.Evaluate(tc.path)
+		if got != tc.want {
+			t.Errorf("%v: got %d, want %d", tc.path, got, tc.want)
+		}
 	}
 }
